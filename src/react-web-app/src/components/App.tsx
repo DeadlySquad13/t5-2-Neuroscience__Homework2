28dc23e36717aa2b0b50d4177f5bdf1b38592770
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-05-27 11:04:36 +0300

    [Enh](react-web-app): Added model loading.

--- src/react-web-app/src/components/App.tsx
+++ src/react-web-app/src/components/App.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import { Input, Button } from "@mui/material";
+import cv from "@techstark/opencv-js";
+import { Tensor, InferenceSession } from "onnxruntime-web";
 
 export const App = () => {
+    const [file, setFile] = useState<File>();
+    const [session, setSession] = useState<InferenceSession>();
+
+    const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const target = event.target as HTMLInputElement;
+        if (target) setFile(target.files![0]);
+    }
+
+    const onFileUpload = async () => {
+        if ((file == undefined)||(file?.type != "image/jpeg")) {
+            console.log("Загрузите изображение в формате jpg");
+            return;
+        }
+        console.log("Изображение успешно загружено");
+    }
+
+    const modelName = "./model.onnx";
+    const modelInputShape = [1, 3, 640, 640];
+
+    cv["onRuntimeInitialized"] = async () => {
+        // create session
+        console.log("Loading YOLOv7 model...");
+        const yolov7 = await InferenceSession.create(modelName);
+
+        // warmup main model
+        console.log("Warming up model...");
+        const tensor = new Tensor(
+            "float32",
+            new Float32Array(modelInputShape.reduce((a, b) => a * b)),
+            modelInputShape
+        );
+        await yolov7.run({ images: tensor });
+
+        setSession(yolov7);
+        console.log("Сессия создана и подготовлена");
+    };
+
     return (
         <div>
-            Пример onnx
+            <div>
+                <Input type="file" onChange={onFileChange}/>
+            </div>
+            <div>
+                <Button variant={"contained"} onClick={onFileUpload}>
+                    Анализ
+                </Button>
+            </div>
         </div>
     )
 }
--- src/react-web-app/webpack.config.js
+++ src/react-web-app/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const paths = {
     src: path.resolve(__dirname, 'src'),
@@ -36,6 +37,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: '../node_modules/onnxruntime-web/dist/*.wasm', to: '[name][ext]' },
+                { from: './model.onnx', to: '[name][ext]' }
+            ]
         }),
     ],
 
